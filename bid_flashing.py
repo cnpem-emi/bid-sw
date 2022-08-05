0638e04f3e76b9b6696c85ef3fa3d5dd2d2aa546
Patricia Nallin <patricia.nallin@gmail.com>
2022-08-05 15:15:24 -0300

    New version

--- bid_flashing.py
+++ bid_flashing.py
@@ -1,49 +1,97 @@
 from asyncio import subprocess
+import argparse
 import pydrs
 import re
 import subprocess
 from xlrd import open_workbook
 
-bid= int(input("Insira o numero da BID ou o nome do UDC "))
 
-datafile = "Inventario.xls"
-sheet = open_workbook(datafile).sheet_by_name("Inventario")
 
-# Procurar a bid na sheet
+IP_DRS = ""
+PORT_DRS = 5000
 
-keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
 
-for row_index in range(1,sheet.nrows):
-    if((sheet.cell(row_index,keys.index("# BID")).value == bid)):
-        device_name = sheet.cell(row_index,keys.index("UDC")).value
-        device_model =sheet.cell(row_index,keys.index("Modelo")).value
-        ps_path = sheet.cell(row_index,keys.index("ps_parameters")).value
-        dsp_path = sheet.cell(row_index,keys.index("dsp_parameters")).value
-        room_name =  device_name[:5]
-        print("PS path: "+ps_path)
-        print("DSP path: "+dsp_path)
-        print("Device name: "+device_name)
-        print("Device model: "+device_model)
-        print("Device room: "+room_name)
 
-def find_dsp_file():
-    if (dsp_path):
-        path = 'udc-dsp-parameters-db/'+room_name+'/fbp/'+dsp_path
-        file = open(path)
-        line = file.readlines()
-        return path
+def read_spreadsheet(datafile = "Inventario.xls", bid = None, pstype = None):
+    sheet = open_workbook(datafile).sheet_by_name("Inventario")
+    keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
+    items = {}
+    for row_index in range(1,sheet.nrows):
+        
+        udc_name = sheet.cell(row_index,keys.index("UDC")).value
+        udc_model =sheet.cell(row_index,keys.index("Modelo")).value
+        ps_file = sheet.cell(row_index,keys.index("ps_parameters")).value
+        dsp_file = sheet.cell(row_index,keys.index("dsp_parameters")).value
+        bid_code = int(sheet.cell(row_index,keys.index("# BID")).value)
 
-def find_ps_file():
-    if(ps_path):
-        path = 'udc-ps-parameters-db/'+room_name+'/fbp/'+ps_path
-        file = open(path)
-        line = file.readlines()
-        return path
+        if (udc_name.split("-")[0] == "IA"):
+            room_name =  udc_name[:5]
+        else:
+            room_name =  udc_name[:2]
 
-if(device_model != "FBP"):
-    print("Modelo selecionado incorreto: " + device_model)
+        if(bid):
+            if((sheet.cell(row_index,keys.index("# BID")).value == bid)):
+                items[udc_name] = [udc_model, ps_file, dsp_file, room_name, bid_code]
+        elif(pstype):
+            if((sheet.cell(row_index,keys.index("Modelo")).value == pstype.upper())):
+                items[udc_name] = [udc_model, ps_file, dsp_file, room_name, bid_code]
+        
+    return items
 
-elif(device_model == "FBP"):
-    print(find_ps_file())
-    #drs =  pydrs.pydrs.EthDRS('10.0.6.59',5000)
-    #drs.set_slave_add(4))
+
+if (__name__ == '__main__'):
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-ps', '--power-supply', dest='ps_type', choices=['fbp', 'fbp-dclink'])
+    parser.add_argument('-bid', '--bid-id', dest='bid_id', type=int)
+    args = parser.parse_args()
+
+ #   drs = pydrs.pydrs.EthDRS(IP_DRS, PORT_DRS)
+    psinfo = {}
+
+    if((args.bid_id is not None) and (args.ps_type is not None)):
+        print("Selecionar apenas a BID ou tipo de fonte!")
+        exit()
+
+    elif(args.ps_type is not None):
+        print("PSType definido!")
+        psinfo = read_spreadsheet(pstype=args.ps_type)
+
+    elif(args.bid_id is not None):
+        print("BID definida!")
+        psinfo = read_spreadsheet(bid=args.bid_id)
+        
+        
+    # psinfo = [udc_model, ps_file, dsp_file, room_name, bid_code]}
+    if psinfo:
+        for ps in psinfo.keys():
+            print(ps, psinfo[ps])
+            ps_param_path = 'udc-ps-parameters-db/{}/{}/{}'.format(psinfo[ps][3], psinfo[ps][0].lower(), psinfo[ps][1])
+            dsp_param_path = 'udc-dsp-parameters-db/{}/{}/{}'.format(psinfo[ps][3], psinfo[ps][0].lower(), psinfo[ps][2])
+            print(ps_param_path)
+            print(dsp_param_path)
+
+
+            confirmation = input("Proceed to Parameters Update?        ( Y ) to continue or  ( N ) to skip UDC/BID\n Enter your choice (N): ")
+            if confirmation == "y" or confirmation == "Y":
+                print("Parameters update process iniciated")
+    #            drs.unlock_udc(0xCAFE)
+    #
+    #            drs.set_param_bank(ps_param_path)
+    #            time.sleep(1)
+    #            drs.save_param_bank(type_memory=1)
+    #            time.sleep(1)
+    #
+    #            drs.set_dsp_modules_bank(dsp_param_path)
+    #            time.sleep(1)
+    #            drs.save_dsp_modules_eeprom(type_memory=1)
+    #            time.sleep(1)
+            else:
+                print("Not updating.\n\n")
+
+
+
+
+# LEITURAS
+# get_param_bank()
+# get_dsp_modules_bank()
