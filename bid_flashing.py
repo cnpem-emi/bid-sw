d2fa009c20b6b46d9cbb42f79bc1d0d77a7a55a3
Patricia Nallin <patricia.nallin@gmail.com>
2022-08-08 12:38:24 -0300

    Updating script. It nows clears and updates BIDs

--- bid_flashing.py
+++ bid_flashing.py
@@ -1,15 +1,52 @@
-from asyncio import subprocess
 import argparse
 import pydrs
-import re
-import subprocess
+import time
 from xlrd import open_workbook
 
+params_01_element = [1, 2, 3, 4, 6, 7, 10, 11, 13, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 33, 34, 35, 36, 37]
+params_04_element = [5, 8, 9, 12, 14, 15, 16, 17, 28, 29, 30, 31, 32, 38, 39, 40, 41, 42, 43, 50, 51]
+params_32_element = [46, 47, 48, 49]
+params_64_element = [0, 44, 45]
 
-
-IP_DRS = ""
+IP_DRS = "10.0.6.59"
 PORT_DRS = 5000
 
+
+def clear_bid():
+    for param in params_01_element:
+        drs.set_param(param, 0, 0)
+
+    for param in params_04_element:
+        for n in range(4):
+            drs.set_param(param, n, 0)
+
+    for param in params_32_element:
+        for n in range(32):
+            drs.set_param(param, n, 0)
+
+    for param in params_64_element:
+        for n in range(64):
+            drs.set_param(param, n, 0)
+
+    # ARM - Defaults
+    drs.set_param(2, 0, 1)
+    drs.set_param(4, 0, 6000000)
+    drs.set_param(5, 0, 1)
+    drs.set_param(5, 1, 30)
+    drs.set_param(5, 2, 30)
+    drs.set_param(5, 3, 30)
+    drs.set_param(6, 0, 1)
+    drs.set_param(10, 0, 1)
+
+
+
+    # \/ MISSING CLEARING DSP & DSP DEFAULTS \/
+
+
+
+    time.sleep(0.5)
+    drs.save_param_bank(type_memory=1)
+    time.sleep(0.5)
 
 
 def read_spreadsheet(datafile = "Inventario.xls", bid = None, pstype = None):
@@ -24,7 +61,7 @@ def read_spreadsheet(datafile = "Inventario.xls", bid = None, pstype = None):
         dsp_file = sheet.cell(row_index,keys.index("dsp_parameters")).value
         bid_code = int(sheet.cell(row_index,keys.index("# BID")).value)
 
-        if (udc_name.split("-")[0] == "IA"):
+        if ("IA-" in udc_name):
             room_name =  udc_name[:5]
         else:
             room_name =  udc_name[:2]
@@ -42,11 +79,12 @@ def read_spreadsheet(datafile = "Inventario.xls", bid = None, pstype = None):
 if (__name__ == '__main__'):
 
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-ps', '--power-supply', dest='ps_type', choices=['fbp', 'fbp-dclink'])
+    parser.add_argument('-ps', '--power-supply', dest='ps_type', choices=['fbp', 'fbp-dclink', 'fap'])
     parser.add_argument('-bid', '--bid-id', dest='bid_id', type=int)
     args = parser.parse_args()
 
- #   drs = pydrs.pydrs.EthDRS(IP_DRS, PORT_DRS)
+    drs = pydrs.EthDRS(IP_DRS, PORT_DRS)
+    drs.slave_addr = 1
     psinfo = {}
 
     if((args.bid_id is not None) and (args.ps_type is not None)):
@@ -62,36 +100,37 @@ if (__name__ == '__main__'):
         psinfo = read_spreadsheet(bid=args.bid_id)
         
         
-    # psinfo = [udc_model, ps_file, dsp_file, room_name, bid_code]}
+    # psinfo[udc_name] = [udc_model, ps_file, dsp_file, room_name, bid_code]
     if psinfo:
         for ps in psinfo.keys():
-            print(ps, psinfo[ps])
             ps_param_path = 'udc-ps-parameters-db/{}/{}/{}'.format(psinfo[ps][3], psinfo[ps][0].lower(), psinfo[ps][1])
             dsp_param_path = 'udc-dsp-parameters-db/{}/{}/{}'.format(psinfo[ps][3], psinfo[ps][0].lower(), psinfo[ps][2])
-            print(ps_param_path)
-            print(dsp_param_path)
 
-
-            confirmation = input("Proceed to Parameters Update?        ( Y ) to continue or  ( N ) to skip UDC/BID\n Enter your choice (N): ")
+            confirmation = input("Flash BID {} for UDC {}?\n( Y ) to continue or  ( N ) to skip UDC/BID\n Enter your choice (N): ".format(psinfo[ps][4], ps))
             if confirmation == "y" or confirmation == "Y":
-                print("Parameters update process iniciated")
-    #            drs.unlock_udc(0xCAFE)
+                
+                drs.unlock_udc(0xCAFE)
+                time.sleep(1)
+
+                print("Clearing BID...")
+                clear_bid()
+
+                print("Saving {} into BID...".format(psinfo[ps][1]))
+                drs.set_param_bank(ps_param_path)
+                time.sleep(0.5)
+                drs.save_param_bank(type_memory=1)
+                time.sleep(0.5)
+
+                if (psinfo[ps][0].lower() != "fbp-dclink"):
+                    print("Saving {} into BID...".format(psinfo[ps][2]))
+                    drs.set_dsp_modules_bank(dsp_param_path)
+                    time.sleep(0.5)
+                    drs.save_dsp_modules_eeprom(type_memory=1)
+                    time.sleep(0.5)
     #
-    #            drs.set_param_bank(ps_param_path)
-    #            time.sleep(1)
-    #            drs.save_param_bank(type_memory=1)
-    #            time.sleep(1)
-    #
-    #            drs.set_dsp_modules_bank(dsp_param_path)
-    #            time.sleep(1)
-    #            drs.save_dsp_modules_eeprom(type_memory=1)
-    #            time.sleep(1)
+    #            LEITURAS
+    #            value = drs.get_param_bank()
+    #            value = drs.get_dsp_modules_bank()
+                #drs.reset_udc()
             else:
-                print("Not updating.\n\n")
-
-
-
-
-# LEITURAS
-# get_param_bank()
-# get_dsp_modules_bank()
+                print("Not updating.\n\n\n")
