7299e89bb17105a350e42611f9479a1e68ad36fc
Patricia Nallin <patricia.nallin@gmail.com>
2022-08-17 18:42:10 -0300

    updates

--- bid_flashing.py
+++ bid_flashing.py
@@ -5,8 +5,64 @@ import time
 from xlrd import open_workbook
 
 
-IP_DRS = "10.0.6.59"
+IP_DRS = "10.0.6.78"
 PORT_DRS = 5000
+FLOAT_MAX_ERROR = 1e-5 # Usually is 1.1920929e-08
+
+
+
+def check_param_bank(csv_file, max_error, memory=1):
+    drs.load_param_bank(type_memory=args.type_mem)
+    read_ps_bank = drs.get_param_bank(print_modules=False)
+    csv_ps_bank = drs.read_csv_param_bank(ps_param_path)
+
+    error_values = {}
+              
+    for param_name in csv_ps_bank.keys():
+        if param_name == "PS_Name":
+            if(csv_ps_bank[param_name] != read_ps_bank[param_name]):
+                print("{} = {} and {} : param differs!".format(
+                    param_name, 
+                    csv_ps_bank[param_name], 
+                    read_ps_bank[param_name],
+                    )
+                )
+        else:
+            for i in range(len(csv_ps_bank[param_name])):
+                if(csv_ps_bank[param_name][i] != read_ps_bank[param_name][i]):
+                    if(abs(csv_ps_bank[param_name][i] - read_ps_bank[param_name][i]) > FLOAT_MAX_ERROR):
+                        print("{}[{}] = {} (CSV) and {} (DRS): params differ!".format(
+                            param_name, 
+                            i, 
+                            csv_ps_bank[param_name][i], 
+                            read_ps_bank[param_name][i],
+                            )
+                        )
+                        error_values[param_name] = {"csv":csv_ps_bank[param_name], "read":read_ps_bank[param_name]}
+    return error_values
+
+def check_dsp_module_bank(csv_file, max_error, memory=1):
+    drs.load_dsp_modules_eeprom(type_memory=memory)
+    read_dsp_bank = drs.get_dsp_modules_bank(print_modules=False)
+    csv_dsp_bank = drs.read_csv_dsp_modules_bank(csv_file)
+
+    error_values = {}
+
+    for param_name in csv_dsp_bank.keys():
+            for ninstance in range(pydrs.consts.num_dsp_modules[pydrs.consts.dsp_classes_names.index(param_name)]):
+                if(csv_dsp_bank[param_name]['coeffs'][ninstance] != read_dsp_bank[param_name]['coeffs'][ninstance]):
+                    for ncoeff in range(pydrs.consts.num_coeffs_dsp_modules[pydrs.consts.dsp_classes_names.index(param_name)]):
+                        if(abs(csv_dsp_bank[param_name]['coeffs'][ninstance][ncoeff] - read_dsp_bank[param_name]['coeffs'][ninstance][ncoeff]) > max_error):
+                            print("{}[{},{}] = {} (CSV) and {} (DRS): params differ!".format(
+                                param_name, 
+                                ninstance,
+                                ncoeff,
+                                csv_dsp_bank[param_name]['coeffs'][ninstance][ncoeff], 
+                                read_dsp_bank[param_name]['coeffs'][ninstance][ncoeff],
+                                )
+                            )
+                            error_values[param_name] = {"csv":csv_dsp_bank[param_name], "read":read_dsp_bank[param_name]}
+    return error_values
 
 
 
@@ -50,9 +106,10 @@ if (__name__ == '__main__'):
 
     memoryType = {1: "BID", 2:"on-board"}
 
-    drs = pydrs.EthDRS(IP_DRS, PORT_DRS)
+    a = drs = pydrs.EthDRS(IP_DRS, PORT_DRS)
     drs.slave_addr = 1
-    psinfo = {}
+
+
 
     if((args.bid_id is not None) and (args.ps_type is not None)):
         print("Selecionar apenas a BID ou tipo de fonte!")
@@ -71,7 +128,7 @@ if (__name__ == '__main__'):
             ps_param_path = 'udc-ps-parameters-db/{}/{}/{}'.format(psinfo[ps][3], psinfo[ps][0].lower(), psinfo[ps][1])
             dsp_param_path = 'udc-dsp-parameters-db/{}/{}/{}'.format(psinfo[ps][3], psinfo[ps][0].lower(), psinfo[ps][2])
 
-            confirmation = input("Flash BID {} for UDC {}?\n( Y ) to continue or  ( N ) to skip UDC/BID\n Enter your choice (N): ".format(psinfo[ps][4], ps))
+            confirmation = input("Flash BID {} for UDC {}? (y/N): ".format(psinfo[ps][4], ps))
             if confirmation == "y" or confirmation == "Y":
                 
                 #print("Clearing BID...")
@@ -79,6 +136,9 @@ if (__name__ == '__main__'):
                 drs.unlock_udc(0xCAFE)
 
 
+                # ------------------------------
+                # FLASHING EEPROM
+                # ------------------------------
                 print("Saving {} into {} memory...".format(psinfo[ps][1], memoryType[args.type_mem]))
                 bid_ps_bank = drs.set_param_bank(ps_param_path)
                 time.sleep(0.5)
@@ -92,21 +152,36 @@ if (__name__ == '__main__'):
                     drs.save_dsp_modules_eeprom(type_memory=args.type_mem)
                     time.sleep(0.5)
 
-    #            LEITURAS
-                drs.load_param_bank(type_memory=args.type_mem)
-                read_ps_bank = drs.get_param_bank(print_modules=False)
-              
-                for param_name in bid_ps_bank.keys():
-                    for i in range(len(bid_ps_bank[param_name])):
-                        if(bid_ps_bank[param_name][i][1] != read_ps_bank[param_name][i][1]):
-                            print("{}[{}] = {} and {} : params differs.\nRetry flashing this BID!".format(param_name, i, bid_ps_bank[param_name][i][0], read_ps_bank[param_name][i][0]))
+                # ------------------------------
+                # RESET UDC FOR PARAMETER LOADING
+                # ------------------------------
+                drs.reset_udc()
 
+
+                # ------------------------------
+                # READINGS - PS PARAMETERS
+                # ------------------------------
+                print("\n\n")
+                print("Loading {} into memory, reading PS parameters and comparing them to {} file".format(
+                    memoryType[args.type_mem],
+                    psinfo[ps][1]
+                ))
+                if(check_param_bank(ps_param_path, FLOAT_MAX_ERROR, memory=args.type_mem)):
+                    print("OOOOOOOOOPS !")
+
+                # ------------------------------
+                # READINGS - DSP PARAMETERS
+                # ------------------------------
                 if (psinfo[ps][0].lower() != "fbp-dclink"):
-                    drs.load_dsp_modules_eeprom(type_memory=args.type_mem)
-                    read_dsp_bank = drs.get_dsp_modules_bank(print_modules=False)
-                    for param_name in bid_dsp_bank.keys():
-                        if(bid_dsp_bank[param_name] != read_dsp_bank[param_name]):
-                            print("{}\n{}\n{}\nparams differs.\nRetry flashing this BID!".format(param_name, bid_dsp_bank[param_name]['coeffs'][0], read_dsp_bank[param_name]['coeffs'][0]))
+                    print("Loading {} into memory, reading DSP parameters and comparing them to {} file".format(
+                    memoryType[args.type_mem],
+                    psinfo[ps][2]
+                ))
+                    if(check_dsp_module_bank(dsp_param_path, FLOAT_MAX_ERROR, memory=args.type_mem)):
+                        print("OOOOOOOOOPS !")
+
 
             else:
                 print("Not updating.\n\n\n")
+
+            
